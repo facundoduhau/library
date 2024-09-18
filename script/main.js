41c2009082677d914204a06e4b1c27784a72278f
facundoduhau <duhaufacundo@gmail.com>
2024-09-17 23:32:28 -0300

    Added function to create new books, working on pop up, need to work on book object to create properties and assign it to each book respectively

--- script/main.js
+++ script/main.js
@@ -1,15 +1,41 @@
+// Page elements.
+
 const newBook = document.querySelector('#newBook');
 const deleteBook = document.querySelector('#deleteBook');
 const favoriteBook= document.querySelector('#favoriteBook');
+const library = document.querySelector('#library');
 
-newBook.addEventListener("click", () => {
-  console.log("Add new book")
-});
+// Declare variables.
 
-deleteBook.addEventListener("click", () => {
-  console.log("Delete this book")
-});
+var bookWrapper;
+var book;
 
-favoriteBook.addEventListener("click", () => {
-  console.log("Favorite this book")
+// Creates the required books elements.
+const createElements = function() {
+
+  bookWrapper = document.createElement('div');
+  book = document.createElement('div');
+}
+
+// Gives the elements their respective class
+const giveClass = () => {
+
+  bookWrapper.classList.add('book-wrapper');
+  book.classList.add('book');
+
+}
+
+// Adds all the appended divs to the "library" element.
+const addBook = () => {
+
+  library.appendChild(bookWrapper); 
+
+}
+
+// Creates new elements, appends them, and later on appends bookWrapper to the "library".
+newBook.addEventListener('click', () => {
+  
+  createElements();
+  giveClass();
+  addBook();
 });
